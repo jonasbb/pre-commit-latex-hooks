dfae1b3ed4ff7078a4a0f4cd39d9cb62f8a45d9e
Jonas Bushart <jonas@bushart.org>
2022-01-19 23:39:29 +0100

    Update version information in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ setup(
     name="LaTeX oriented pre-commit hooks",
     description="Contains hook(s) for pre-commit, see http://pre-commit.com",
     author="Jonas Bushart <jonas@bushart.org>",
-    version="1.1.0",
+    version="1.2.5",
     classifiers=[
         "License :: OSI Approved :: Apache 2 License",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(),
     entry_points={
